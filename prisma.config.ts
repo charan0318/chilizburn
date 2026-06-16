b3e8391075ee25937796f2b149ec43173c023148
charan0318 <chve0402@gmail.com>
2026-06-16 18:52:58 +0530

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  generator: {
-    provider: "prisma-client-js",
-  },
   datasource: {
-    provider: "postgresql",
-    url: process.env.DIRECT_URL,
+    url: process.env.DATABASE_URL ?? process.env.DIRECT_URL,
   },
 });
